ed78fa45994cb6e41eb46cc9105ee21a268f6cb3
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2022-03-26 17:24:21 +0500

    Create bookstore dependecies

---
+++ pkg/config/app.go
@@ -0,0 +1,19 @@
+package config
+
+import "github.com/jinzhu/gorm"
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:admin/book-store?charset=utf8&parseTime=True&loc=local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
